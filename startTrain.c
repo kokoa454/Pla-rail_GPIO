81a0f75abf07f089aec7692c6681c619c58ee11c
Inari Goro <qqnq7um9k@gmail.com>
2025-07-02 14:35:23 +0900

    updated the grammer of all files

--- startTrain.c
+++ startTrain.c
@@ -1,6 +1,7 @@
 #include "Plarail_GPIO.h"
 
-void startTrain(int iHndl){
+void startTrain(int iHndl)
+{
     lgGpioWrite(iHndl, SIG, LG_HIGH);
     outputLog("列車を発車させました");
     return;
--- stopTrain.c
+++ stopTrain.c
@@ -1,6 +1,7 @@
 #include "Plarail_GPIO.h"
 
-void stopTrain(int iHndl){
+void stopTrain(int iHndl)
+{
     lgGpioWrite(iHndl, SIG, LG_LOW);
     outputLog("列車を停止させました");
     return;
